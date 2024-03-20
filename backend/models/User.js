eac86de95e10b311414ae208bc676ab3bd095a85
dearalina <dearalina2020@gmail.com>
2024-03-20 14:52:25 -0700

    feat: create user, item, cart models

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validator(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
+});
+
+module.exports = mongoose.model('User', userSchema);
