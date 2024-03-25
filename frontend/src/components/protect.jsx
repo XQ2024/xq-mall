0a15af4e877b97a917bd67e59bf3fb1022815bed
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-03-25 19:19:13 -0400

    Update protect.jsx

--- frontend/src/components/protect.jsx
+++ frontend/src/components/protect.jsx
@@ -3,8 +3,6 @@ import { useMemo } from "react";
 
 export default function ProtectedLayout({ children }) {
   const user = useMemo(() => localStorage.getItem("user"), []);
-  // const navigate = useNavigate();
-  //假设后端有API拿到一个JWT token，放入localStorage里
 
   if (!user) {
     //如果没登录
@@ -15,8 +13,6 @@ export default function ProtectedLayout({ children }) {
   return (
     <div>
       {children}
-      {/* {children}  作为 <ProtectedLayout> 组件的子组件传递进来的任何内容，<User /> */}
-      {/* <Outlet /> */}
     </div>
   );
 }
